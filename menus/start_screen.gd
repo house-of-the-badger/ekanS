cb0a3225a3d1b1cd8c9cc41addb36997a3388685
AnyaVeselova <anna.correspondence@outlook.com>
2024-07-04 01:22:21 +0100

    styles pause menu and hud

--- menus/start_screen.gd
+++ menus/start_screen.gd
@@ -8,10 +8,12 @@ const gameplay_scene:PackedScene = preload("res://gameplay/gameplay.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	var high_score:int = Global.save_data.high_score
 	score.text = "High Score: " + str(high_score)
 	#Global.save_data.high_score = 10
 	#Global.save_data.save()
+
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_packed(gameplay_scene)
