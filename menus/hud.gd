21f45619fff2f81b260047feed9cbcd756c88097
AnyaVeselova <anna.correspondence@outlook.com>
2024-07-05 00:29:58 +0100

    merges recent UI changes to the most recent branch (Josh's); connects UI scenes; fixes bugs; refactors code; adds call_deffered to line 32 in gameplay.gd; changes the signal in the hud and puts a fanction for decreasing snake length into gameplay

--- menus/hud.gd
+++ menus/hud.gd
@@ -1,9 +1,9 @@
 class_name HUD extends CanvasLayer
+signal decrease_snake_length
+
 
 @onready var score = $Panel/MarginContainer/HBoxContainer/Score
 @onready var snake_length = $Panel/MarginContainer/HBoxContainer/SnakeLength
-
-
 
 var new_snake_length = Global.starting_snake_length
 
@@ -11,11 +11,9 @@ func _ready():
 	snake_length.text = "Snake Length: " + str(new_snake_length)
 	
 	
-	
 func update_score(n:int):
-	score.text = "Score: " + str(n)
-	
-func _on_gameplay_decrease_snake_length():
 	new_snake_length -= 1
 	snake_length.text = "Snake Length: " + str(new_snake_length)
-	print(new_snake_length)
+	
+	
+	
