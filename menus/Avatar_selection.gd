1426a751c2ba8de166d44255a2efa884fdc690c4
XiaoQuark <kris.oldrini@gmail.com>
2024-07-08 08:43:08 +0100

    connect game to database

--- menus/Avatar_selection.gd
+++ menus/Avatar_selection.gd
@@ -1,68 +1,72 @@
 class_name AvatarSelection extends Control
 
+@onready var get_avatars = %GetAvatars
 
 var COLLECTION_NAME_PLAYERS = "players"
-#var COLLECTION_NAME_AVATARS = "avatars"
-#var DOCUMENT_ID = "avatar_1"
-@onready var snake_1 = %Snake1
-@onready var snake_2 = %Snake2
-@onready var snake_3 = %Snake3
-var avatar_id 
-var avatar_img
-
-
+var avatar_id:int
+var avatar_img:String
 
 func _ready():
-	#var collection: FirestoreCollection = Firebase.Firestore.collection(COLLECTION_NAME_AVATARS)
-	#var document = await collection.get_doc(DOCUMENT_ID)
-	#print(document)	
 	pass
-
-
-func _process(delta):
-	pass
-
-
-func _on_snake_1_pressed():
-	avatar_id = snake_1
-	avatar_img = "gs://project-ekans.appspot.com/snake1.png"
-
-
-func _on_snake_2_pressed():
-	avatar_id = snake_2
-	avatar_img = "gs://project-ekans.appspot.com/snake2.png"
-
-
-func _on_snake_3_pressed():
-	avatar_id = snake_3
-	avatar_img = "gs://project-ekans.appspot.com/snake3.png"
-
 
 func _on_confirm_button_pressed():
 	save_data()
+	get_tree().change_scene_to_file("res://menus/user_profile.tscn")
 
 func save_data():
 	var auth = Firebase.Auth.auth
-	print(auth.localid)
 	if auth.localid:
 		var firestore_collection: FirestoreCollection= Firebase.Firestore.collection(COLLECTION_NAME_PLAYERS)
 		var username = %Username.text
 		
 		var document = await firestore_collection.get_doc(auth.localid)
-		if document:
+		print(document)
+		if document && not document.is_null_value("username"):
+			print("Document exists, updating fields")
+			%UsernameStateLabel.text = "Your snake was updated"
 			document.add_or_update_field("username", username)
 			document.add_or_update_field("avatar_id", avatar_id)
 			document.add_or_update_field("avatar_img", avatar_img)
 			await firestore_collection.update(document)
 		else:
+			print("Document does not exist, creating new document")
+			%UsernameStateLabel.text = "Your snake was saved"
 			var data : Dictionary= {
 			"username":username,
 			"avatar_id":avatar_id,
 			"avatar_img":avatar_img,
 		}
-			document = await firestore_collection.add(auth.localid, 		data)
-
+			document = await firestore_collection.add(auth.localid, data)
 
 func _on_logout_button_pressed():
 	Firebase.Auth.logout()
-	get_tree().change_scene_to_file("res://menus/SignUp.tscn")
+	get_tree().change_scene_to_file("res://menus/Login.tscn")
+
+
+func _on_avatar_1_pressed():
+	set_avatar(1, "avatars/avatar_1.png")
+
+
+func _on_avatar_2_pressed():
+	set_avatar(2, "avatars/avatar_2.png")
+
+
+func _on_avatar_3_pressed():
+	set_avatar(3, "avatars/avatar_3.png")
+
+
+func _on_avatar_4_pressed():
+	set_avatar(4, "avatars/avatar_4.png")
+
+
+func _on_avatar_5_pressed():
+	set_avatar(5, "avatars/avatar_5.png")
+
+
+func _on_avatar_6_pressed():
+	set_avatar(6, "path/to/avatar_6.png")
+
+func set_avatar(id:int, img_path:String):
+	avatar_id = id
+	avatar_img = img_path
+	
--- menus/sign-up.gd
+++ menus/sign-up.gd
@@ -5,11 +5,6 @@ class_name SignUp extends Control
 func _ready(): 
 	Firebase.Auth.signup_succeeded.connect(on_signup_succeeded)
 	Firebase.Auth.signup_failed.connect(on_signup_failed)
-	Firebase.Auth.login_succeeded.connect(on_login_succeeded)
-	Firebase.Auth.login_failed.connect(on_login_failed)
-
-
-
 
 func _on_sign_up_button_pressed():
 	var email = %EmailInput.text
@@ -17,30 +12,20 @@ func _on_sign_up_button_pressed():
 	var confirm_password = %PasswrodInput2.text
 	if(password == confirm_password):
 		Firebase.Auth.signup_with_email_and_password(email, password)
-		%StateLabel.text = "Creating account..."
+		%SignupStateLabel.text = "Creating account..."
 	else:
-		%StateLabel.text = "Password don't match"
+		%SignupStateLabel.text = "Password don't match"
 
 func on_signup_succeeded(auth):
 	print(auth)
-	%StateLabel.text= "Account created!"
+	%SignupStateLabel.text= "Account created!"
 	Firebase.Auth.save_auth(auth)
 	get_tree().change_scene_to_file("res://menus/Avatar_selection.tscn")
 	
-
 func on_signup_failed(error_code, message):
 	print(error_code)
 	print(message)
-	%StateLabel.text = "%s" % message
-
+	%SignupStateLabel.text = "%s" % message
 
 func _on_login_button_pressed():
-	var email = %EmailInput.text
-	var password = %PasswordInput.text
-	Firebase.Auth.login_with_email_and_password(email, password)
-	
-func on_login_succeeded(auth):
-	get_tree().change_scene_to_file("res://menus/Avatar_selection.tscn")
-	
-func on_login_failed(error_code, message):
-	%StateLabel.text = "%s" % message
+	get_tree().change_scene_to_file("res://menus/Login.tscn")
