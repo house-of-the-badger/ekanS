e23d906a8fd2a15054bc59e7f3235e70d3dac9b8
XiaoQuark <kris.oldrini@gmail.com>
2024-07-08 15:05:31 +0100

    fix the database connection

--- menus/sign-up.gd
+++ menus/sign-up.gd
@@ -1,5 +1,6 @@
 class_name SignUp extends Control
 
+var COLLECTION_NAME_PLAYERS = "players"
 
 # Called when the node enters the scene tree for the first time.
 func _ready(): 
@@ -17,15 +18,16 @@ func _on_sign_up_button_pressed():
 		%SignupStateLabel.text = "Password don't match"
 
 func on_signup_succeeded(auth):
-	print(auth)
 	%SignupStateLabel.text= "Account created!"
 	Firebase.Auth.save_auth(auth)
+	await DatabaseManager.create_empty_player_document(auth.localid)
 	get_tree().change_scene_to_file("res://menus/Avatar_selection.tscn")
 	
 func on_signup_failed(error_code, message):
-	print(error_code)
-	print(message)
+	print(error_code, "signup error code")
+	print(message, "signup error message")
 	%SignupStateLabel.text = "%s" % message
 
 func _on_login_button_pressed():
 	get_tree().change_scene_to_file("res://menus/Login.tscn")
+
